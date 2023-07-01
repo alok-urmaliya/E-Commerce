9f67378667fcc62fcfa014894f597057e4c9b7e4
praveenpathak <praveenpathak235@gmail.com>
2023-07-01 18:21:57 +0530

    Add to SignUp and SignIn Page

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,25 @@
-﻿namespace E_Commerce;
+﻿using Microsoft.Maui.Platform;
+using E_Commerce.Handlers;
+using E_Commerce;
+
+namespace E_Commerce;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new AppShell();
-	}
+    public App()
+    {
+        InitializeComponent();
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderlessEntry), (handler, view) =>
+        {
+            if (view is BorderlessEntry)
+            {
+#if __ANDROID__
+                handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+#elif __IOS__
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            }
+        });
+        MainPage = new AppShell();
+    }
 }
